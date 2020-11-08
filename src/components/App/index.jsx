fd0eab7dfed8538a277819d011817a9a72489012
hare <hendrasadewa30@gmail.com>
2020-11-08 16:23:35 +0700

    fix unused contents warning

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,21 +2,7 @@
 import { Global } from '@emotion/core';
 import MainMenu from '../MainMenu';
 
-import Quiz from '../Quiz';
-
 import { appStyles, globalStyles } from './style';
-
-// Mock quiz
-const mockQuizProps = {
-  question: 'Kuala Lumpur is the capital of which country?',
-  correctAnswer: 'Malaysia',
-  choices: [
-    { label: 'A', answer: 'Vietnam' },
-    { label: 'B', answer: 'Indonesia' },
-    { label: 'C', answer: 'Malaysia' },
-    { label: 'D', answer: 'Thailand' },
-  ],
-};
 
 function App() {
   return (
