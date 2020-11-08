b1b4caec2241f756cf2647b958cf7f082ebc288a
hare <hendrasadewa30@gmail.com>
2020-11-08 14:57:48 +0700

    styling quiz component

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -2,22 +2,45 @@
 import Box from '../Box';
 import ChoiceButton from '../ChoiceButton';
 
-import { choiceContainerStyle, questionStyle } from './styles';
+// assets
+import adventureImageSrc from '../../assets/images/undraw_adventure_4hum 1.svg';
 
-function Quiz({ question, choices }) {
+import {
+  questionContainerStyle,
+  titleStyle,
+  choiceContainerStyle,
+  questionStyle,
+  headerIllustrationStyle,
+  countryFlagStyle,
+} from './styles';
+
+function Quiz({ question, flag, choices }) {
   return (
-    <Box>
-      <section>
-        <p css={questionStyle}>{question}</p>
-      </section>
-      <section>
-        {choices.map(({ label, answer }) => (
-          <div css={choiceContainerStyle} key={`choice-${label}`}>
-            <ChoiceButton label={label} answer={answer} />
-          </div>
-        ))}
-      </section>
-    </Box>
+    <div css={questionContainerStyle}>
+      <h1 css={titleStyle}>Country Quiz</h1>
+      <Box>
+        <section>
+          <img
+            src={adventureImageSrc}
+            alt="adventure illustration"
+            css={headerIllustrationStyle}
+          />
+        </section>
+        <section>
+          {!!flag && (
+            <img alt="country flag" src={flag} css={countryFlagStyle} />
+          )}
+          <p css={questionStyle}>{question}</p>
+        </section>
+        <section>
+          {choices.map(({ label, answer }) => (
+            <div css={choiceContainerStyle} key={`choice-${label}`}>
+              <ChoiceButton label={label} answer={answer} />
+            </div>
+          ))}
+        </section>
+      </Box>
+    </div>
   );
 }
 
--- src/components/Quiz/styles.js
+++ src/components/Quiz/styles.js
@@ -1,7 +1,32 @@
 import { css } from '@emotion/core';
 
+const questionContainerStyle = css`
+  position: relative;
+  z-index: 1;
+`;
+
+const titleStyle = css`
+  color: white;
+  padding: 8px 16px;
+  margin: 0;
+  font-size: 36px;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+const headerIllustrationStyle = css`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
+
 const choiceContainerStyle = css`
   margin-bottom: 18px;
+`;
+
+const countryFlagStyle = css`
+  width: 84px;
+  height: 54px;
 `;
 
 const questionStyle = css`
@@ -11,4 +36,11 @@ const questionStyle = css`
   color: #2f527b;
 `;
 
-export { choiceContainerStyle, questionStyle };
+export {
+  questionContainerStyle,
+  titleStyle,
+  headerIllustrationStyle,
+  choiceContainerStyle,
+  countryFlagStyle,
+  questionStyle,
+};
