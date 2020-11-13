1e17803290f46d8e6754faa5d75ce42486f7cfcb
hare <hendrasadewa30@gmail.com>
2020-11-13 22:47:58 +0700

    manage app statte

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,43 +1,32 @@
 /** @jsxImportSource @emotion/core */
 import { Global } from '@emotion/core';
-import { useState } from 'react';
-import axios from 'axios';
+import { useEffect, useReducer } from 'react';
 
 import BoxLayout from '../../components/BoxLayout';
 import Game from '../Game';
 
 import { appStyles, globalStyles } from './style';
+import appReducer, { APP_STATUS, initialState, thunks } from './states';
 
 function App() {
-  const [appStatus, setAppStatus] = useState('idle');
-  const [countryData, setCountryData] = useState([]);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const { data, error, status } = state;
 
-  useState(() => {
-    if (appStatus === 'idle') {
-      const params = {
-        fields: ['name', 'capital', 'flag'].join(';'),
-      };
-
-      axios
-        .get('https://restcountries.eu/rest/v2/all', { params })
-        .then((response) => {
-          setCountryData(response.data);
-          setAppStatus('ready');
-        })
-        .catch((error) => {
-          setError(error.message || 'unknown error');
-          setAppStatus('error');
-        });
+  useEffect(() => {
+    if (status === APP_STATUS.IDLE) {
+      thunks.startApp(dispatch);
     }
-  }, [appStatus]);
+  }, [status]);
 
   return (
     <>
       <Global styles={globalStyles} />
       <main css={appStyles}>
         <BoxLayout>
-          {appStatus === 'ready' && <Game countryData={countryData} />}
+          {status === APP_STATUS.IDLE && <p>Waiting for action</p>}
+          {status === APP_STATUS.PENDING && <p>Getting ready . . .</p>}
+          {status === APP_STATUS.READY && <Game countryData={data} />}
+          {status === APP_STATUS.ERROR && <p>{error.message}</p>}
         </BoxLayout>
       </main>
     </>
